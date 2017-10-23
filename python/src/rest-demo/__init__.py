adc23d6c96e7938239fba8f58b5516fa2aacfe60
maxwu <maxwunj@gmail.com>
2017-10-23 20:53:47 +1300

    add python baseline

---
+++ python/src/rest-demo/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Public Title.
+ Doc str for module users
+
+ .. moduleauthor:: Max Wu <http://maxwu.me>
+ 
+ .. References::
+    **Stackoverflow**, link to problem statement.
+    **Hackerrank**, link to problem statement.
+    
+ .. Test Samples in doctest format
+>>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+"""
+
+if __name__ == "__main__":
+    pass
